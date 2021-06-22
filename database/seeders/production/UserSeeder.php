14faa0e0bb757835e5c4025c7176ee1b4afc18ac
Sora <skyarts0122@gmail.com>
2021-06-22 10:21:16 +0900

    🐛Fix: conflictコードの削除

--- database/seeders/production/UserSeeder.php
+++ database/seeders/production/UserSeeder.php
@@ -4,10 +4,8 @@ namespace Database\Seeders\production;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-<<<<<<< HEAD
 use Illuminate\Support\Facades\Hash;
-=======
->>>>>>> 05ae81cc0df8520fb0a50a29e63254327733f836
+
 
 class UserSeeder extends Seeder
 {
@@ -26,11 +24,7 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'ゲスト',
             'email' => 'guest@user.com',
-<<<<<<< HEAD
             'password' => Hash::make(config('data.ADMIN_PASSWORD')),
-=======
-
->>>>>>> 05ae81cc0df8520fb0a50a29e63254327733f836
             'role' => 'guest',
             'api_token' => 1234,
         ]);
